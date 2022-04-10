aef083497b977e0375b582b1f3baa16a415afc00
openmetrue <openmetrue@gmail.com>
2022-04-10 20:55:42 +0800

    Initial Commit

---
+++ Cats/CatsApp.swift
@@ -0,0 +1,17 @@
+//
+//  CatsApp.swift
+//  Cats
+//
+//  Created by Mark Khmelnitskii on 10.04.2022.
+//
+
+import SwiftUI
+
+@main
+struct CatsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Cats/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Cats
+//
+//  Created by Mark Khmelnitskii on 10.04.2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
